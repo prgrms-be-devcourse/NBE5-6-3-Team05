4f9ddff63485bc92a0da3e210df1f43e63c743ea
Leeka99 <dsrn0000@gmail.com>
2025-05-09 14:08:50 +0900

    (feat) change layout

--- src/main/java/com/grepp/moodlink/app/controller/web/IndexController.java
+++ src/main/java/com/grepp/moodlink/app/controller/web/IndexController.java
@@ -2,7 +2,9 @@ package com.grepp.moodlink.app.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/recommend")
 @Controller
 public class IndexController {
     @GetMapping
