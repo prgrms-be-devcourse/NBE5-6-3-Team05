be569bbf1ac61e39e0f91fddae5f060f6100b2d3
Kminseo <childstone@users.noreply.github.com>
2025-05-13 15:17:52 +0900

    (feat) 도서 추가 구현 (이미지구현x)
    이미지 경로 수정 필요합니다.

---
+++ src/main/java/com/grepp/moodlink/app/controller/web/admin/payload/BookAddRequest.java
@@ -0,0 +1,40 @@
+package com.grepp.moodlink.app.controller.web.admin.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.grepp.moodlink.app.model.data.book.code.Genre;
+
+import com.grepp.moodlink.app.model.data.book.dto.BookDto;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class BookAddRequest {
+    private List<MultipartFile> image;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String author;
+    @NotBlank
+    private String genre;
+
+    private String publisher;
+
+    private String description;
+
+    //front에서 LocalDate로 넘어옴
+    private LocalDate publishedDate;
+
+    public BookDto toDto(){
+        BookDto bookDto= new BookDto();
+        bookDto.setTitle(title);
+        bookDto.setAuthor(author);
+        bookDto.setGenre(genre);
+        bookDto.setPublisher(publisher);
+        bookDto.setDescription(description);
+        bookDto.setPublishedDate(publishedDate);
+        return bookDto;
+    }
+}
--- src/main/java/com/grepp/moodlink/app/model/data/book/BookService.java
+++ src/main/java/com/grepp/moodlink/app/model/data/book/BookService.java
@@ -2,15 +2,57 @@ package com.grepp.moodlink.app.model.data.book;
 
 import com.grepp.moodlink.app.model.data.book.dto.BookDto;
 import com.grepp.moodlink.app.model.data.book.entity.Book;
+import com.grepp.moodlink.infra.error.exceptions.CommonException;
+import com.grepp.moodlink.infra.response.ResponseCode;
+import com.grepp.moodlink.infra.util.file.FileDto;
+import com.grepp.moodlink.infra.util.file.FileUtil;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final FileUtil fileUtil;
+    private final ModelMapper mapper;
+
+    // 관리자 페이지에서 도서 추가
+    @Transactional
+    public void addBook(List<MultipartFile> thumbnail, BookDto dto) {
+
+        if(bookRepository.existsByTitleAndAuthor(dto.getTitle(),dto.getAuthor()))
+            throw new CommonException(ResponseCode.DUPLICATED_DATA);
+
+        try {
+            List<FileDto> fileDtos = fileUtil.upload(thumbnail, "book");
+            Book book = mapper.map(dto, Book.class);
+
+            if(!fileDtos.isEmpty()){
+                FileDto fileDto = fileDtos.getFirst();
+                String renameFileName = fileDto.renameFileName();
+                String savePath = fileDto.savePath();
+
+                book.setImage("/download/" + savePath + renameFileName);
+            }
+
+            long count = bookRepository.count();
+            book.setIsbn("B"+count);
+
+            log.info("{}",book);
+
+            bookRepository.save(book);
+        } catch (IOException e) {
+            throw new CommonException(ResponseCode.INTERNAL_SERVER_ERROR, e);
+        }
+    }
 
     public void saveMusic(List<BookDto> bookDtos) {
 
@@ -28,6 +70,13 @@ public class BookService {
 
             bookRepository.save(book);
         }
+    }
+
+
+    public List<BookDto> findAll() {
+        return bookRepository.findAll()
+            .stream().map(e -> mapper.map(e, BookDto.class))
+            .toList();
     }
 
 }
