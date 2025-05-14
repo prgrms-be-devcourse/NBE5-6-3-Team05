0738c32584cc80098c48b6b9efebabff291267ae
Kminseo <childstone@users.noreply.github.com>
2025-05-14 12:27:17 +0900

    (fix) JpaRepository 타입 잘못 기재 수정
    long->String

--- src/main/java/com/grepp/moodlink/app/model/data/book/BookRepository.java
+++ src/main/java/com/grepp/moodlink/app/model/data/book/BookRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long>, BookRepositoryCustom {
+public interface BookRepository extends JpaRepository<Book, String>, BookRepositoryCustom {
 
     List<Book> findByEmbeddingIsNull();
 
