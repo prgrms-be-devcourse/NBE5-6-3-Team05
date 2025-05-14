f763313b92f72a2aa09aa2fb51c254bcb3e7d26d
Kminseo <childstone@users.noreply.github.com>
2025-05-14 21:20:18 +0900

    (feat) Movie 삭제 구현

--- src/main/java/com/grepp/moodlink/app/controller/api/admin/AdminApiController.java
+++ src/main/java/com/grepp/moodlink/app/controller/api/admin/AdminApiController.java
@@ -1,6 +1,8 @@
 package com.grepp.moodlink.app.controller.api.admin;
 
 import com.grepp.moodlink.app.model.data.book.BookService;
+import com.grepp.moodlink.app.model.data.movie.MovieRepository;
+import com.grepp.moodlink.app.model.data.movie.MovieService;
 import com.grepp.moodlink.infra.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminApiController {
 
     private final BookService bookService;
+    private final MovieService movieService;
 
+    // 영화 삭제
+    @DeleteMapping("movies/delete/{id}")
+    public ResponseEntity<String> deleteMovie(@PathVariable String id) {
+        log.info("삭제합니다!!");
+        movieService.deleteMovie(id);
+        log.info("삭제완료~~~~~~~~~~~~");
+        return ResponseEntity.ok("정상적으로 삭제되었습니다.");
+    }
+
+
+    // 도서 삭제
     @DeleteMapping("books/delete/{isbn}")
     public ResponseEntity<String> deleteBook(@PathVariable String isbn) {
         log.info("삭제합니다!!");
