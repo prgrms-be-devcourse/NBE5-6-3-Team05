4818d88dfe4df174211a91c8ae362dbaa42049c4
Kminseo <childstone@users.noreply.github.com>
2025-05-15 12:09:56 +0900

    (feat) Image 경로 수정
    현재 디렉토리로 이미지 생성

--- src/main/java/com/grepp/moodlink/infra/util/file/FileUtil.java
+++ src/main/java/com/grepp/moodlink/infra/util/file/FileUtil.java
@@ -1,8 +1,9 @@
 package com.grepp.moodlink.infra.util.file;
 
-import jakarta.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,27 +14,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileUtil {
-    
+
     @Value("${upload.path}")
-    private String filePath;
+    private String uploadPath;
 
-    private String absoluteUploadPath;
-
-    @PostConstruct
-    public void init() {
-        File file = new File(filePath);
-        absoluteUploadPath = file.getAbsolutePath();
-        new File(absoluteUploadPath).mkdirs();
-    }
-    
     public List<FileDto> upload(List<MultipartFile> files, String depth) throws IOException {
         List<FileDto> fileDtos = new ArrayList<>();
-        
+
         if (files == null || files.isEmpty() || files.getFirst().isEmpty()) {
             return fileDtos;
         }
         String savePath = createSavePath(depth);
-        
+
         for (MultipartFile file : files) {
             String originFileName = file.getOriginalFilename();
             String renameFileName = generateRenameFileName(originFileName);
@@ -41,25 +33,28 @@ public class FileUtil {
             fileDtos.add(fileDto);
             uploadFile(file, fileDto);
         }
-        
+
         return fileDtos;
     }
-    
+
     private void uploadFile(MultipartFile file, FileDto fileDto) throws IOException {
-        File path = new File(absoluteUploadPath + fileDto.savePath());
-        if (!path.exists()) {
-            path.mkdirs();
+        Path projectRoot = Paths.get("").toAbsolutePath();
+        Path fullUploadPath = projectRoot.resolve(uploadPath).resolve(fileDto.savePath());
+
+        File directory = fullUploadPath.toFile();
+        if (!directory.exists()) {
+            directory.mkdirs();
         }
-        
-        File target = new File(absoluteUploadPath + fileDto.savePath() + fileDto.renameFileName());
+
+        File target = new File(fullUploadPath.toFile(), fileDto.renameFileName());
         file.transferTo(target);
     }
-    
+
     private String generateRenameFileName(String originFileName) {
         String ext = originFileName.substring(originFileName.lastIndexOf("."));
         return UUID.randomUUID().toString() + ext;
     }
-    
+
     private String createSavePath(String depth) {
         LocalDate now = LocalDate.now();
         return depth + "/" +
@@ -67,5 +62,5 @@ public class FileUtil {
                    now.getMonth() + "/" +
                    now.getDayOfMonth() + "/";
     }
-    
+
 }
