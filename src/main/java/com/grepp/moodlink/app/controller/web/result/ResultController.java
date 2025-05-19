39724315e5a5b1547fbff50d1fa6dc8f8f3c2a17
june3780 <112941412+june3780@users.noreply.github.com>
2025-05-19 11:43:35 +0900

    (reat) RecommendController의 session결과를 ResultController에 인가

--- src/main/java/com/grepp/moodlink/app/controller/web/result/ResultController.java
+++ src/main/java/com/grepp/moodlink/app/controller/web/result/ResultController.java
@@ -4,7 +4,7 @@ package com.grepp.moodlink.app.controller.web.result;
 import com.grepp.moodlink.app.model.result.ResultService;
 import com.grepp.moodlink.app.model.result.dto.CuratingDetailDto;
 import com.grepp.moodlink.app.model.result.dto.CuratingDetailIdDto;
-import java.util.ArrayList;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,33 +23,25 @@ public class ResultController {
     private final ResultService resultService;
 
     @GetMapping
-    public String result(Model model, Authentication authentication) {
+    public String result(Model model, Authentication authentication, HttpSession session) {
         String userId ="";
         if (authentication != null){
             userId = authentication.getName();
         }
 
-        // temp data start
-        CuratingDetailIdDto curatingDetailIdDto1 = new CuratingDetailIdDto("M51", "S51", "B51");
-        CuratingDetailIdDto curatingDetailIdDto2 = new CuratingDetailIdDto("M52", "S52", "B52");
-        CuratingDetailIdDto curatingDetailIdDto3 = new CuratingDetailIdDto("M53", "S53", "B53");
-        CuratingDetailIdDto curatingDetailIdDto4 = new CuratingDetailIdDto("M54", "S54", "B54");
-        List<CuratingDetailIdDto> recommendResult = new ArrayList<>();
-        recommendResult.add(curatingDetailIdDto1);
-        recommendResult.add(curatingDetailIdDto2);
-        recommendResult.add(curatingDetailIdDto3);
-        recommendResult.add(curatingDetailIdDto4);
-        // temp data end
+        List<CuratingDetailIdDto> recommendResult = (List<CuratingDetailIdDto>) session.getAttribute("items");
+        if (recommendResult == null){
+            recommendResult = List.of();
+        }
 
-        // example: curating Reason start
-        String msg1 ="curatingReason출력문 예시 <br>";
-        String msg2 ="오늘 해고당해서 우울한 당신! 고단한 하루를 달래줄 명작을 소개해드릴게요. <br>";
-        String msg3 ="다음 작품들은 위로가 될 거예요!";
-        // example: curating Reason end
+        String reason = (String) session.getAttribute("reason");
+        if (reason == null){
+            reason = "기본 이유입니다.아이유";
+        }
 
         List<CuratingDetailDto> tempList = resultService.curatingDetailDtoList(userId, recommendResult);
 
-        model.addAttribute("curatingReason", msg1+msg2+msg3);
+        model.addAttribute("curatingReason", reason);
         model.addAttribute("items", tempList);
         return "result/result";
     }
