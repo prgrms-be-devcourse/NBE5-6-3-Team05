57401f5569d2d5d10f716a2fcdc98dfffb08625c
Kminseo <childstone@users.noreply.github.com>
2025-06-04 10:43:11 +0900

    feat: 책, 음악 장르 entity 추가 및 기존 entity 수정

---
+++ src/main/java/com/grepp/moodlink/app/model/data/book/entity/BookGenre.java
@@ -0,0 +1,24 @@
+package com.grepp.moodlink.app.model.data.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book_genre")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String name;
+
+}
---
+++ src/main/java/com/grepp/moodlink/app/model/data/music/entity/MusicGenre.java
@@ -0,0 +1,24 @@
+package com.grepp.moodlink.app.model.data.music.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "music_genre")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MusicGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String name;
+
+}
