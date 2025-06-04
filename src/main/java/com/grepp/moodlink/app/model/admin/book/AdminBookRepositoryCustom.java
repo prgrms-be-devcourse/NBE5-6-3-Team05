7c19107019805df2f0f0377331484c22e49e2bc9
Kminseo <childstone@users.noreply.github.com>
2025-06-04 10:46:21 +0900

    (refactor) update 로직 수정

--- src/main/java/com/grepp/moodlink/app/model/admin/book/AdminBookRepositoryCustom.java
+++ src/main/java/com/grepp/moodlink/app/model/admin/book/AdminBookRepositoryCustom.java
@@ -9,7 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminBookRepositoryCustom {
 
-    void updateBook(BookDto book);
-
     Page<Book> findPaged(Pageable pageable);
 }
