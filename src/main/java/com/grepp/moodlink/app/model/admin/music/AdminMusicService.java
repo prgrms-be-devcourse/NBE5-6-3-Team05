d78acb127b73a6e70b3790dba302c61552b80f3f
Kminseo <childstone@users.noreply.github.com>
2025-06-06 21:11:42 +0900

    (feat) 음악 장르 추가 삭제 수정 구현

--- src/main/java/com/grepp/moodlink/app/model/admin/music/AdminMusicService.java
+++ src/main/java/com/grepp/moodlink/app/model/admin/music/AdminMusicService.java
@@ -1,7 +1,8 @@
 package com.grepp.moodlink.app.model.admin.music;
 
-import com.grepp.moodlink.app.model.data.music.MusicGenreRepository;
+import com.grepp.moodlink.app.model.data.movie.entity.Genre;
 import com.grepp.moodlink.app.model.data.music.dto.MusicDto;
+import com.grepp.moodlink.app.model.data.music.dto.MusicGenreDto;
 import com.grepp.moodlink.app.model.data.music.entity.Music;
 import com.grepp.moodlink.app.model.data.music.entity.MusicGenre;
 import com.grepp.moodlink.app.model.llm.EmbeddingService;
@@ -12,8 +13,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -28,7 +31,8 @@ public class AdminMusicService {
     private final AdminMusicRepository musicRepository;
     private final EmbeddingService embeddingService;
     private final ImgUploadTemplate imgUploadTemplate;
-    private final MusicGenreRepository musicGenreRepository;
+    private final AdminMusicGenreRepository musicGenreRepository;
+    private final ModelMapper mapper;
 
     // 음악 목록을 페이지네이션으로 가져옴
     public Page<MusicDto> findPaged(Pageable pageable) {
@@ -123,8 +127,6 @@ public class AdminMusicService {
             // 업데이트
             musicRepository.save(music);
             embeddingService.generateEmbeddingsMusic();
-            log.info("{}", dto);
-
 
         } catch (IOException e) {
             throw new CommonException(ResponseCode.INTERNAL_SERVER_ERROR, e);
@@ -138,7 +140,47 @@ public class AdminMusicService {
     }
     
     // 모든 음악 장르를 가져옴
-    public List<String> findAllGenre() {
+    public List<String> findAllGenreName() {
         return musicGenreRepository.findAll().stream().map(MusicGenre::getName).toList();
+    }
+
+    public List<MusicGenreDto> findAllGenre() {
+        return musicGenreRepository.findAllByActivated(true).stream().map(e->mapper.map(e,MusicGenreDto.class)).collect(
+            Collectors.toList());
+    }
+
+    // 해당 장르를 사용하는 음악 수 카운트
+    public Long countMusicByGenre(Long id) {
+        return musicRepository.countMusicByGenre_Id(id);
+    }
+
+    // 장르 추가
+    @Transactional
+    public void addGenre(MusicGenreDto musicGenreDto) {
+        if(musicGenreRepository.findByName(musicGenreDto.getName())!=null){
+            throw new CommonException(ResponseCode.DUPLICATED_DATA);
+        }
+        MusicGenre musicGenre = new MusicGenre(null, musicGenreDto.getName());
+        musicGenreRepository.save(musicGenre);
+    }
+
+    // 장르 삭제
+    @Transactional
+    public Boolean deleteGenre(Long id) {
+        // 해당 장르인 음악이 하나라도 있다면 삭제 불가능
+        if(countMusicByGenre(id)!=0)
+            return false;
+        musicGenreRepository.findById(id).ifPresent(MusicGenre::unActivated);
+        return true;
+    }
+
+    // 장르 수정
+    @Transactional
+    public void modifyGenre(Long longId, MusicGenreDto musicGenreDto) {
+
+        MusicGenre genre = musicGenreRepository.findById(longId).get();
+        genre.setName(musicGenreDto.getName());
+        musicGenreRepository.save(genre);
+
     }
 }
