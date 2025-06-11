e99c62de4b959db4a02442e7dbf88d3cde074f24
june3780 <112941412+june3780@users.noreply.github.com>
2025-06-11 22:55:39 +0900

    (feat) 음악, 도서에 대한 월드컵도 생성 가능

---
+++ src/main/java/com/grepp/moodlink/app/controller/api/book/BookApiController.java
@@ -0,0 +1,27 @@
+package com.grepp.moodlink.app.controller.api.book;
+
+
+import com.grepp.moodlink.app.model.data.book.BookService;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("api/book")
+public class BookApiController {
+
+    private final BookService bookService;
+
+    // worldcup 생성 시 도서 목록 전달(추후 LLM으로 변경 예정)
+    @GetMapping("/worldcup")
+    public ResponseEntity<List<Map<String,Object>>> getBooks(){
+        return ResponseEntity.ok(bookService.getBookList());
+    }
+}
--- src/main/java/com/grepp/moodlink/app/model/data/book/BookService.java
+++ src/main/java/com/grepp/moodlink/app/model/data/book/BookService.java
@@ -2,10 +2,10 @@ package com.grepp.moodlink.app.model.data.book;
 
 import com.grepp.moodlink.app.model.data.book.dto.BookDto;
 import com.grepp.moodlink.app.model.data.book.entity.Book;
-import com.grepp.moodlink.infra.error.exceptions.CommonException;
-import com.grepp.moodlink.infra.response.ResponseCode;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -84,5 +84,20 @@ public class BookService {
         Book book = bookRepository.findByIsbn(isbn);
         Long currentCount = book.getLikeCount();
         book.setLikeCount(currentCount - 1);
+    }
+
+    public List<Map<String, Object>> getBookList() {
+        // Dto 대신 임의의 Map<>을 통해 전달("id" : 도서 컨텐츠의 id, "title": 도서 컨텐츠의 제목)
+        return bookRepository.findAll().stream()
+            .map(m -> {
+
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", m.getIsbn());
+                map.put("title", m.getTitle());
+
+                // 함수형 내부에서의 return(메서드의 반환이 아님.)
+                return map;
+            })
+            .collect(Collectors.toList());
     }
 }
