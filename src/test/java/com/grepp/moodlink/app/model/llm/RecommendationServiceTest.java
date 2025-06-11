58a945373df1e9ececf22252ab2163c895505da9
kdg010729 <kdg010729@naver.com>
2025-06-11 15:08:48 +0900

    fix. 안쓰는 import 삭제

--- src/test/java/com/grepp/moodlink/app/model/llm/RecommendationServiceTest.java
+++ src/test/java/com/grepp/moodlink/app/model/llm/RecommendationServiceTest.java
@@ -1,6 +1,5 @@
 package com.grepp.moodlink.app.model.llm;
 
-import com.grepp.moodlink.app.model.data.movie.MovieService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
